b0b08f868f0742bb27493e0132abc859f2479fbb
BadDwagon <38072174+BadDwagon@users.noreply.github.com>
2024-12-12 23:23:46 -0500

    Fixed bump reminder

--- events/bump.js
+++ events/bump.js
@@ -5,27 +5,27 @@ module.exports = {
     name: Events.MessageCreate,
     once: false,
     execute: async (message) => {
-        try {
-            let member = message.interaction.user.toString();
+        if (message.embeds[0] == undefined) return;
+        const member = message.interaction.user.toString();
 
-            if (message.embeds[0].description.startsWith('Bump')) {
-                await message.channel.send({
-                    content: `${en.bump.message.thanks_first} ${member} ${en.bump.message.thanks_second}`
+        if (message.embeds[0].description.startsWith('Bump')) {
+            const reply = en.events.bump.response.newTimer
+            await message.channel.send({
+                content: reply.replace(/%Arg%/, member)
+            });
+
+            setTimeout(async () => {
+                const bumpTimeEmbed = new EmbedBuilder()
+                    .setTitle(en.events.bump.response.newTimer.title)
+                    .setURL(`https://disboard.org/server/${message.guild.id}`)
+                    .setDescription(en.events.bump.response.newTimer.description)
+                    .setColor('Blue');
+
+                return message.channel.send({
+                    content: member,
+                    embeds: [bumpTimeEmbed]
                 });
-
-                return setTimeout(async () => {
-                    let bumpTimeEmbed = new EmbedBuilder()
-                        .setTitle(en.bump.message.embed.title)
-                        .setURL(`https://disboard.org/server/${message.guild.id}`)
-                        .setDescription(en.bump.message.embed.description)
-                        .setColor('Blue');
-
-                    return message.channel.send({
-                        content: `${member}`,
-                        embeds: [bumpTimeEmbed]
-                    });
-                }, 7200000);
-            };
-        } catch (error) { return }
+            }, 7200000);
+        };
     }
 };
