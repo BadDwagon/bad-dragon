dff6511a0b4c480fd3c1eefd544958a4c24f45f3
BadDwagon <38072174+BadDwagon@users.noreply.github.com>
2024-12-12 23:33:05 -0500

    Added try instead of checking for any possiblity

--- events/bump.js
+++ events/bump.js
@@ -6,25 +6,28 @@ module.exports = {
     once: false,
     execute: async (message) => {
         if (message.embeds[0] == undefined) return;
-        if (message.embeds[0].description.startsWith('Bump')) {
-            const member = message.interaction.user.toString();
-            const reply = en.events.bump.response.newTimer
-            await message.channel.send({
-                content: reply.replace(/%Arg%/, member)
-            });
 
-            setTimeout(async () => {
-                const bumpTimeEmbed = new EmbedBuilder()
-                    .setTitle(en.events.bump.response.newTimer.title)
-                    .setURL(`https://disboard.org/server/${message.guild.id}`)
-                    .setDescription(en.events.bump.response.newTimer.description)
-                    .setColor('Blue');
-
-                return message.channel.send({
-                    content: member,
-                    embeds: [bumpTimeEmbed]
+        try {
+            if (message.embeds[0].description.startsWith('Bump')) {
+                const member = message.interaction.user.toString();
+                const reply = en.events.bump.response.newTimer
+                await message.channel.send({
+                    content: reply.replace(/%Arg%/, member)
                 });
-            }, 7200000);
-        };
+
+                setTimeout(async () => {
+                    const bumpTimeEmbed = new EmbedBuilder()
+                        .setTitle(en.events.bump.response.newTimer.title)
+                        .setURL(`https://disboard.org/server/${message.guild.id}`)
+                        .setDescription(en.events.bump.response.newTimer.description)
+                        .setColor('Blue');
+
+                    return message.channel.send({
+                        content: member,
+                        embeds: [bumpTimeEmbed]
+                    });
+                }, 7200000);
+            };
+        } catch (error) { return; };
     }
 };
